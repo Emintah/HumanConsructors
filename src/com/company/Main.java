6c1d09cbb3e38a7ad8f8018bc7938f19fb30f5b1
emintah <emintah@ruc.dk>
2021-09-15 11:47:53 +0200

    human constructors

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+     Human human1 = new Human("rick", 65,70);
+     Human human2 = new Human("lisa", 23, 50);
+        System.out.println(human2.name);
+    }
+}
+
+
