02d92818b823786d7805b49f285a3a842e321174
emintah <emintah@ruc.dk>
2021-09-15 11:50:54 +0200

    this code added tobias to the code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
      Human human1 = new Human("rick", 65,70);
      Human human2 = new Human("lisa", 23, 50);
+      Human human3 = new Human("Tobias", 5, 200);
         System.out.println(human2.name);
     }
 }
